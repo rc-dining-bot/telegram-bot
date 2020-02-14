863d902269181785c4a0bce6f3ebecdb0931e7b0
Mo Zongran <mozongran@gmail.com>
2020-02-14 09:31:15 +0800

    Change psycopg2 in Pipfile to binary

--- src/scheduler/scheduler_config.py
+++ src/scheduler/scheduler_config.py
@@ -3,7 +3,7 @@ from util.util import localized_time
 # broadcast timings
 BREAKFAST_BROADCAST_TIME = localized_time(hour=0,
                                           minute=0,
-                                          second=0)
+                                          second=1)
 DINNER_BROADCAST_TIME = localized_time(hour=15,
                                        minute=0,
                                        second=0)
