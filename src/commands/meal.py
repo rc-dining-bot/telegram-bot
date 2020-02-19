f22c81f7b7d5de0151987bdeb01f0baa649f13ba
Mo Zongran <mozongran@gmail.com>
2020-02-19 16:45:27 +0800

    Replace inline callback reply with edit text

--- src/commands/meal.py
+++ src/commands/meal.py
@@ -10,7 +10,6 @@ from util.messages import no_menu_msg, menu_msg, failed_to_parse_date_msg
 from util.util import parse_menu, localized_date_today
 from database.database import get_raw_menu, get_hidden_cuisines
 from util.kb_mark_up import start_button_kb
-from datetime import date
 from dateparser import parse
 
 
@@ -29,24 +28,37 @@ def handle_menu(meal):
         parsed_date = get_menu_query_date(entered_date)
 
         if parsed_date is None:
-            context.bot.send_message(chat_id=chat_id,
-                                     text=failed_to_parse_date_msg(entered_date))
+            if update.callback_query is not None:
+                context.bot.edit_message_text(chat_id=chat_id,
+                                              message_id=update.callback_query.message.message_id,
+                                              text=failed_to_parse_date_msg(entered_date))
+            else:
+                context.bot.send_message(chat_id=chat_id,
+                                         text=failed_to_parse_date_msg(entered_date))
             return
 
         menu = get_raw_menu(meal, parsed_date)
         hidden_cuisines = get_hidden_cuisines(update.effective_chat.id)
 
         if menu is None:  # if no menu, reply with no menu message
-            context.bot.send_message(chat_id=chat_id,
-                                     text=no_menu_msg(meal),
-                                     reply_markup=start_button_kb())
+            if update.callback_query is not None:
+                context.bot.edit_message_text(chat_id=chat_id,
+                                              message_id=update.callback_query.message.message_id,
+                                              text=no_menu_msg(meal),
+                                              reply_markup=start_button_kb())
+            else:
+                context.bot.send_message(chat_id=chat_id,
+                                         text=no_menu_msg(meal),
+                                         reply_markup=start_button_kb())
+
         else:  # else reply user of the menu
             menu = menu_msg(parsed_date, meal, parse_menu(menu, hidden_cuisines))
             # send formatted menu to client
-            context.bot.send_message(chat_id=chat_id,
-                                     text=menu,
-                                     parse_mode=telegram.ParseMode.HTML,
-                                     reply_markup=start_button_kb())
+            context.bot.edit_message_text(chat_id=chat_id,
+                                          message_id=update.callback_query.message.message_id,
+                                          text=menu,
+                                          parse_mode=telegram.ParseMode.HTML,
+                                          reply_markup=start_button_kb())
         logging.info(f"{chat_id}: {meal} menu sent to chat")
 
     def get_menu_query_date(entered_date):
--- src/commands/settings.py
+++ src/commands/settings.py
@@ -23,23 +23,35 @@ from util.messages import (
 def handle_settings(update, context):
     if update.callback_query is not None:
         context.bot.answer_callback_query(update.callback_query.id)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=settings_msg(),
-                             reply_markup=settings_kb(),
-                             parse_mode=telegram.ParseMode.HTML)
+        context.bot.edit_message_text(chat_id=update.effective_chat.id,
+                                      message_id=update.callback_query.message.message_id,
+                                      text=settings_msg(),
+                                      reply_markup=settings_kb(),
+                                      parse_mode=telegram.ParseMode.HTML)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=settings_msg(),
+                                 reply_markup=settings_kb(),
+                                 parse_mode=telegram.ParseMode.HTML)
 
 
 # hidden cuisine panel
 def handle_hidden_cuisine(update, context):
-    if update.callback_query is not None:
-        context.bot.answer_callback_query(update.callback_query.id)
     hidden_cuisine = get_hidden_cuisines(update.effective_chat.id)
     msg = no_hidden_cuisine_msg() if not hidden_cuisine else hidden_cuisine_msg(update.effective_chat.first_name)
 
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=msg,
-                             reply_markup=hidden_cuisine_kb(hidden_cuisine),
-                             parse_mode=telegram.ParseMode.HTML)
+    if update.callback_query is not None:
+        context.bot.answer_callback_query(update.callback_query.id)
+        context.bot.edit_message_text(chat_id=update.effective_chat.id,
+                                      message_id=update.callback_query.message.message_id,
+                                      text=msg,
+                                      reply_markup=hidden_cuisine_kb(hidden_cuisine),
+                                      parse_mode=telegram.ParseMode.HTML)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=msg,
+                                 reply_markup=hidden_cuisine_kb(hidden_cuisine),
+                                 parse_mode=telegram.ParseMode.HTML)
 
 
 # update keyboard markup after updating hide cuisine panel
@@ -53,13 +65,18 @@ def handle_hide_cuisine(update, context):
 
 
 def handle_notification(update, context):
-    if update.callback_query is not None:
-        context.bot.answer_callback_query(update.callback_query.id)
     chat_id = update.effective_chat.id
     bf_sub, dn_sub = get_subscribe_setting(chat_id)
-    context.bot.send_message(chat_id=chat_id,
-                             text=notification_view_msg(),
-                             reply_markup=notification_kb(bf_sub=bf_sub, dn_sub=dn_sub))
+    if update.callback_query is not None:
+        context.bot.answer_callback_query(update.callback_query.id)
+        context.bot.edit_message_text(chat_id=chat_id,
+                                      message_id=update.callback_query.message.message_id,
+                                      text=notification_view_msg(),
+                                      reply_markup=notification_kb(bf_sub=bf_sub, dn_sub=dn_sub))
+    else:
+        context.bot.send_message(chat_id=chat_id,
+                                 text=notification_view_msg(),
+                                 reply_markup=notification_kb(bf_sub=bf_sub, dn_sub=dn_sub))
 
 
 def handle_subscribe(meal):
