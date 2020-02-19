eba0005c3bbdedff9eba3eaa50a7d1c79e8b6cc8
Mo Zongran <mozongran@gmail.com>
2020-02-19 23:28:16 +0800

    Add meal credit login page

--- src/util/kb_mark_up.py
+++ src/util/kb_mark_up.py
@@ -12,6 +12,7 @@ def start_kb():
         InlineKeyboardButton(text="Breakfast Menu", callback_data="menu.breakfast"),
         InlineKeyboardButton(text="Dinner Menu",    callback_data="menu.dinner"),
         InlineKeyboardButton(text="Settings",       callback_data="settings.home"),
+        InlineKeyboardButton(text="Meal credit (Login required)", url="https://aces.nus.edu.sg/Prjhml/"),
         InlineKeyboardButton(text="Help",           callback_data="start.help"),
     ]
     return InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
